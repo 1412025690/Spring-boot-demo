a0f13e02c70967c7a3140dd1cad6957707511770
xqz <1412025690@qq.com>
2022-05-31 23:43:22 +0800

    2022/05/31 23:43

--- src/main/java/com/kkb/controller/UserController.java
+++ src/main/java/com/kkb/controller/UserController.java
@@ -2,6 +2,7 @@ package com.kkb.controller;
 
 import com.kkb.domain.User;
 import com.kkb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Controller
 public class UserController {
+    @Autowired
     private UserService userService;
 
     @RequestMapping("/all")
@@ -17,6 +19,6 @@ public class UserController {
         List<User> list = userService.findAll();
         model.addAttribute("users",list);
         // 跳转classpath:/templates/users.html
-        return "users";
+        return "user";
     }
 }
